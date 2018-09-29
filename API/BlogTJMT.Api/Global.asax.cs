370fac72600a655dafb538d7d3c374cfbb239d9e
cristhyanko <cristhyan.ko@outlook.com>
2018-09-28 21:21:36 -0400

    Removendo arquivos desncessários e fazendo as devidas alterações

--- API/BlogTJMT.Api/Global.asax.cs
+++ API/BlogTJMT.Api/Global.asax.cs
@@ -16,8 +16,6 @@ namespace BlogTJMT.Api
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
